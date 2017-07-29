4b91679d17c0f87b239803c071f6ba270367a790
Falk Hoppe <falkhoppe81@gmail.com>
2017-07-29 07:50:56 +0200

    fix auth routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   get 'pages/landing_page'
 
 
+  get '/auth/:provider/callback', to: 'admin/sessions#create'
+  get 'auth/failure', to: redirect('/edit/sessions/new')
+
+
   namespace 'edit', module: 'admin', as: 'admin' do
     get 'sessions/new'
 
     # authentication
     get 'sessions/destroy'
-    get '/auth/:provider/callback', to: 'sessions#create'
-    get 'auth/failure', to: redirect('/new')
     delete 'signout', to: 'sessions#destroy', as: 'signout'
 
     get '/', to: 'pages#index'
