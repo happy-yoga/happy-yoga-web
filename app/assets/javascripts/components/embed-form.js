c70a347df2019b2134ade5970351ba27046d81a4
Falk Hoppe <falkhoppe81@gmail.com>
2017-07-30 14:15:51 +0200

    fix embed form and preview

--- app/assets/javascripts/components/embed-form.js
+++ app/assets/javascripts/components/embed-form.js
@@ -13,30 +13,52 @@ class EmbedForm extends HTMLElement {
 
   connectedCallback () {
     this.root = $(this)
-    this.form = this.root.find('form')
-    this.form.on('submit', (e) => {
-      e.preventDefault()
-
-      fetch(this.form.attr('action'), {
-        method: this.form.attr('method') || 'GET',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: this.form.serialize(),
-        credentials: 'include'
-      })
-        .then(res => res.text())
-        .then(text => {
-          const content = this.selector ? resolveSelector(text, this.selector) : text
-          this.root.html(content)
-        })
-        .catch(e => {
-          console.log(e)
-        })
-    })
+    this.initializeForm()
   }
 
   disconnetedCallback () {
+  }
+
+  initializeForm () {
+    console.log('initialize Form')
+    this.form = this.root.find('form')
+    this.form.on('submit', (e) => {
+      e.preventDefault()
+      this.serializeForm()
+    })
+  }
+
+  serializeForm (e) {
+    const body = this.form.serialize()
+    const method = this.form.attr('method') || 'GET'
+    this.disableForm()
+
+    return fetch(this.form.attr('action'), {
+      method,
+      body,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      credentials: 'include'
+    })
+      .then(res => res.text())
+      .then(text => {
+        const content = this.selector ? resolveSelector(text, this.selector) : text
+        this.root.html(content)
+      })
+      .then(() => this.initializeForm())
+      .catch(e => {
+        this.enableForm()
+        console.log(e)
+      })
+  }
+
+  disableForm () {
+    this.form.find(':input').prop('disabled', true)
+  }
+
+  enableForm () {
+    this.form.find(':input').prop('disabled', false)
   }
 
   set root (elem) {
