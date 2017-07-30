b773d25551d9c46ec7e5d2ebc0e1fdfd43fae300
Falk Hoppe <falkhoppe81@gmail.com>
2017-07-30 12:11:36 +0200

    split preview and embed component

--- app/assets/javascripts/components/embed-form.js
+++ app/assets/javascripts/components/embed-form.js
@@ -1,64 +1,14 @@
-/* global window, customElements, HTMLElement, $ */
-
-const preview = () => `<embed-form-preview></embed-form-preview>`
-
-const bindDimensions = function bindDimensions (reference, element) {
-  element
-    .css('height', reference.css('height'))
-    .css('width', reference.css('width'))
-}
+/* global customElements, HTMLElement */
 
 class EmbedForm extends HTMLElement {
-  static get dimensionCheckInterval () { return 1000 }
-
   constructor (self) {
     self = super(self)
-    this.dom = {}
   }
 
   connectedCallback () {
-    this.root = $(this)
-    this.textArea = this.root.find('textarea')
-    this.preview = $(preview())
-
-    this.bindPreviewDimensions()
-    this.textArea.after(this.preview)
   }
 
   disconnetedCallback () {
-    window.clearInterval(this.previewInterval)
-  }
-
-  bindPreviewDimensions () {
-    bindDimensions(this.textArea, this.preview)
-
-    this.previewInterval = setInterval(() =>
-      bindDimensions(this.textArea, this.preview), EmbedForm.dimensionCheckInterval
-    )
-  }
-
-  set root (root) {
-    this.dom.root = root
-  }
-
-  get root () {
-    return this.dom.root
-  }
-
-  set textArea (textArea) {
-    this.dom.textArea = textArea
-  }
-
-  get textArea () {
-    return this.dom.textArea
-  }
-
-  set preview (preview) {
-    this.dom.preview = preview
-  }
-
-  get preview () {
-    return this.dom.preview
   }
 }
 
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,1 +1,2 @@
+import './components/preview-text-area.js'
 import './components/embed-form.js'
