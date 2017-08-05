e13912cf4562c14024cfc6f23291d16cdb593355
Falk Hoppe <falkhoppe81@gmail.com>
2017-08-05 08:14:35 +0200

    add md rendering and akrobat font

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def landing_page
+    @welcome_text = Content.find_by(name: "landing_page_welcome_text")
     render layout: 'landing_page'
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def markdown(text)
+    return markdown_renderer.render(text).html_safe
+  end
+
+  private
+
+  def markdown_renderer
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+      no_intra_emphasis: true,
+      disable_indented_code_blocks: true,
+      autolink: true,
+      tables: true,
+      underline: true,
+      highlight: true
+    )
+  end
 end
